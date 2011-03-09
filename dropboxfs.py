760e93d8717659d8e43faa87ef6c4dc23dd9c7af
revolunet <julien@bouquillon.com>
2011-03-09 20:15:08 +0100

    multiple fixes

--- dropboxfs.py
+++ dropboxfs.py
@@ -36,7 +36,7 @@ import StringIO
 
 import dropbox.auth, dropbox.client
 
-
+ 
 
 import datetime
 
@@ -53,6 +53,7 @@ dropBoxConfig = {
     }
     
 class myStringIO(StringIO.StringIO, object):
+    # simple wrapper due to DropBox that need a 'physical' file
     def __init__(self, name, data):
         self.data = data
         super(myStringIO, self).__init__(  self.data )
@@ -68,24 +69,41 @@ class _DropBoxFSFile(object):
 
     """ A file-like that provides access to a file with FileBrowser API"""
 
-    def __init__(self, dropboxfs, path, mode):
+    def __init__(self, dropboxfs, path, mode = 'r'):
+        if not path.startswith('/'):
+            path = u'/%s' % path
         self.dropboxfs = dropboxfs
-        self.path = normpath(path)
+        self.path = path
         self.mode = mode
         self.closed = False
         self.file_size = 0
         #if 'r' in mode or 'a' in mode:
          #   self.file_size = dropboxfs.getsize(path)
+         
+    def getCacheDir(self, dir = False):
+        if dir:
+            return self.path
+        root = os.path.split(self.path)[0]
+        if root == '':
+            root = '/'
+        return root
+        
+    def getFullPath(self):
+        return self.dropboxfs.getDropBoxFullPath( self.path )
 
     def read(self):
         # read dropboxfile
-        resp = self.dropboxfs.dropBoxCommand('get_file', path = self.path)
-        return resp.read()
+        if self.dropboxfs.isfile(self.path):
+            resp = self.dropboxfs.dropBoxCommand('get_file', path = self.path)
+            return resp.read()
+        else:
+            return False
         
     def write(self, data):
-        # write dropboxfile
+        # write to dropbox
         cfile = myStringIO(self.path, data)
         resp = self.dropboxfs.dropBoxCommand('put_file', path = self.path, data = cfile)
+        self.dropboxfs.refreshDirCache( os.path.split(self.path)[0] )
         return (resp.status == 200)
 
     def close(self):
@@ -112,28 +130,15 @@ class DropBoxFS(FS):
     def __init__(self, **kwargs): 
                     
         # base settings
-        self.dropBoxConfig = dropBoxConfig
-       
+        self.config = dropBoxConfig
+        self.client = None
+        
+        # cache folder infos
+        self.cache_paths = {}
+        
         # update with settings
-        self.dropBoxConfig.update(  **kwargs )
+        self.config.update(  **kwargs )
 
-        self.cache_paths = {}
-       
-    # def urlopen( self, data, get = {}, headers = {}):
-        # if self.username and self.username!='' and self.password and self.password!='':
-            # passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-            # passman.add_password(None, self.root_url, self.username, self.password)
-            # authhandler = urllib2.HTTPBasicAuthHandler(passman)
-            # opener = urllib2.build_opener(authhandler)
-            # urllib2.install_opener(opener)
-        
-        # get = get and ('?'+urlencode(get)) or ""
-        # r = urllib2.urlopen( urllib2.Request(self.root_url + get, urlencode(data), headers ) )
-        # return r
-        
-    def cacheReset(self):
-        self.cache_paths = {}
-        
  
     def getsize(self, path):
         item = self.__getNodeInfo( path )
@@ -142,7 +147,7 @@ class DropBoxFS(FS):
         return 0
 
     def _check_path(self, path):
-        path = normpath(path)
+        path = (path)
         base, fname = pathsplit(abspath(path))
         
         dirlist = self._readdir(base)
@@ -152,14 +157,12 @@ class DropBoxFS(FS):
 
     def getinfo(self, path, overrideCache = False):
         node = self.__getNodeInfo(path, overrideCache = overrideCache)
-        #node['modified_time'] = datetime.datetime.fromtimestamp(node['modified_time'])
-        #node['created_time'] = node['modified_time']
         return node
 
         
     def open(self, path, mode="r"):
 
-        path = normpath(path)
+        path = (path)
         mode = mode.lower()        
         if self.isdir(path):
             raise ResourceInvalidError(path)        
@@ -170,7 +173,7 @@ class DropBoxFS(FS):
             # if not self.isfile(path):
                 # raise ResourceNotFoundError(path)
       
-        f = _DropBoxFSFile(self, normpath(path), mode) 
+        f = _DropBoxFSFile(self, (path), mode) 
       
         return f 
  
@@ -179,10 +182,9 @@ class DropBoxFS(FS):
         return self.isfile(path) or self.isdir(path)
     
     def isdir(self, path):
-        if path==self.dropBoxConfig.get('path', '/'):
+        if path in ['', '/']:
             return True
         item = self.__getNodeInfo( path )
-        #print item
         if item:
             # attribute may not be present in the JSON for dirs
             return (item.get('is_dir') == True)
@@ -191,7 +193,7 @@ class DropBoxFS(FS):
 
     
     def isfile(self, path):
-        if path==self.dropBoxConfig.get('path', '/'):
+        if path in ['', '/']:
             return False
         item = self.__getNodeInfo( path )
         if item:
@@ -200,30 +202,25 @@ class DropBoxFS(FS):
             return False
 
     def makedir(self, path, recursive=False, allow_recreate=False):
-        path = normpath(path)
         if path in ('', '/'):
             return
-        # DROPBOX MAKEDIR
         resp = self.dropBoxCommand('file_create_folder', path=path)
-        self.refreshDirCache( path )
+        self.refreshDirCache( os.path.split(path)[0] )
         return (resp.status==200)
         
         
     def rename(self, src, dst, overwrite=False, chunk_size=16384):
         # if not overwrite and self.exists(dst):
             # raise DestinationExistsError(dst)
-        # DROPBOX RENAME
-        
         resp = self.dropBoxCommand('file_move', from_path=src, to_path=dst)
-        self.refreshDirCache( src )
-        self.refreshDirCache( dst )
+        self.refreshDirCache( os.path.split(src)[0] )
+        self.refreshDirCache( os.path.split(dst)[0] )
          
         return (resp.status==200)
 
     def refreshDirCache(self, path):
-        (root1, file) = self.__getBasePath( path )
         # reload cache for dir
-        self.listdir(root1, overrideCache=True)
+        self.listdir(path, overrideCache=True)
 
     def removedir(self, path):        
         if not self.isdir(path):
@@ -233,43 +230,30 @@ class DropBoxFS(FS):
     def remove(self, path, checkFile = True):        
         # DROPBOX DELETE 
         resp = self.dropBoxCommand('file_delete', path=path)
-        self.refreshDirCache( path )
+        self.refreshDirCache( os.path.split(path)[0] )
         return (resp.status==200)
-        
-    def __getBasePath(self, path):
-        parts = path.split('/')
-        root = self.dropBoxConfig.get('path', '/')
-        file = path
-        if len(parts)>1:
-            root += '/'+('/'.join(parts[:-1]))
-            file = parts[-1]
-        if file == '/': file=''
-        if root == '//': root='/'
-        return root, file
-        
-        
+ 
     def __getNodeInfo(self, path, overrideCache = False):
         # check if file exists in cached data or fecth target dir
-        
-        (root, file) = self.__getBasePath( path )
-        
-        cache = self.cache_paths.get( root )
+        f = _DropBoxFSFile(self, path)
+        cachedir = f.getCacheDir()
+        fpath = f.getFullPath()
         # check if in cache
+        cache = self.cache_paths.get( cachedir )
         item = None
         if cache and not overrideCache:
-            item = [item for item in cache if item['path']==(root+file)] or None
+            item = [item for item in cache if item['path']==fpath] or None
             if item: 
                 item = item[0]
         else:
             # fetch listdir in cache then restart
-            res = self.listdir( root )
+            res = self.listdir( cachedir )
             if res:
                 item = self.__getNodeInfo( path )
+                pass
         return item
             
-    def close(self):
-        # for cacheFS   
-        pass
+ 
         
     def listdir(self, path="/",
                       wildcard=None,
@@ -279,74 +263,80 @@ class DropBoxFS(FS):
                       files_only=False,
                       overrideCache=False
                       ):
-        # if not path.startswith('/'):
-            # path=u'/%s' % path
         if not path.startswith('/'):
-            path = '/'+path
-        if path!=self.dropBoxConfig.get('path') and path.rstrip('/')!=self.dropBoxConfig.get('path'):
-            path=self.dropBoxConfig.get('path') + path
-        cache = self.cache_paths.get(path)
+            path=u'/%s' % path
+             
+        f = _DropBoxFSFile(self, path)
+        cachedir = f.getCacheDir(dir=True)
+ 
+        root = self.config.get('path', '/')
+   
+        fdir = self.getDropBoxFullPath(path)
+
+        cache = self.cache_paths.get( cachedir )
         if cache and not overrideCache:
             flist = [f['path'][f['path'].rfind('/')+1:] for f in cache]
         else:
-            json = self.dropBoxCommand('metadata', path=path)
+            json = self.dropBoxCommand('metadata', path=path )
             flist = []
             if json and json.has_key('contents'):
                 d = json['contents']
-                if d:
-                    self.cache_paths[path] = d
-                    flist = [f['path'][f['path'].rfind('/')+1:] for f in d]
-                
+                self.cache_paths[cachedir] = d
+                flist = [f['path'][f['path'].rfind('/')+1:] for f in d]
+
         alist = self._listdir_helper('/', flist, wildcard, full, absolute, dirs_only, files_only)
         return alist
     
     def getDropBoxClient(self):
-        auth = dropbox.auth.Authenticator( self.dropBoxConfig )
-        token = auth.obtain_trusted_access_token(self.dropBoxConfig['user_name'], self.dropBoxConfig['user_password'])
-        c = dropbox.client.DropboxClient(self.dropBoxConfig['server'], self.dropBoxConfig['content_server'], self.dropBoxConfig['port'], auth, token)
+        auth = dropbox.auth.Authenticator( self.config )
+        token = auth.obtain_trusted_access_token(self.config['user_name'], self.config['user_password'])
+        c = dropbox.client.DropboxClient(self.config['server'], self.config['content_server'], self.config['port'], auth, token)
         return c
+ 
+        
+    def getDropBoxFullPath(self, inPath):
+        path = self.config['path']
 
-    def dropBoxPath(self, path):
-        root = self.dropBoxConfig['path']
-        if path and not path.startswith(root):
-            if not root.endswith('/') and not path.startswith('/'):
-                root += '/'
-            root += path
-        return root
+        if inPath not in ['/','']:
+            if path == '/':
+                path=''
+            p = inPath
+            if not p.startswith('/'):
+                p = '/%s' % p
+            path += p
+        return path
         
     def dropBoxCommand(self, cmd, **kwargs):
+        path = self.getDropBoxFullPath( kwargs.get('path','') ) 
+ 
         if not self.client:
             self.client = self.getDropBoxClient()
         if cmd == 'metadata':
-            path =  kwargs.get('path','')         
-            resp = self.client.metadata(self.dropBoxConfig['root'], path, list=True, status_in_response=False, callback=None)
+            #print 'metadata', path
+            resp = self.client.metadata(self.config['root'], path, list=True, status_in_response=False, callback=None)
             if resp.status==200:
                 return simplejson.loads(resp.body)
         elif cmd == 'file_create_folder':
-            path = self.dropBoxPath(kwargs.get('path',''))
             #print 'file_create_folder', path
-            resp = self.client.file_create_folder(self.dropBoxConfig['root'], path)
+            resp = self.client.file_create_folder(self.config['root'], path)
             return resp
         elif cmd == 'file_delete':
-            path = self.dropBoxPath(kwargs.get('path',''))
             #print 'file_delete', path
-            resp = self.client.file_delete(self.dropBoxConfig['root'], path)
+            resp = self.client.file_delete(self.config['root'], path)
             return resp
         elif cmd == 'file_move':
-            from_path = self.dropBoxPath(kwargs.get('from_path',''))
-            to_path = self.dropBoxPath(kwargs.get('to_path',''))
+            from_path = self.getDropBoxFullPath( kwargs.get('from_path','') ) 
+            to_path = self.getDropBoxFullPath( kwargs.get('to_path','') ) 
             #print 'file_move', from_path, to_path
-            resp = self.client.file_move(self.dropBoxConfig['root'], from_path, to_path)
+            resp = self.client.file_move(self.config['root'], from_path, to_path)
             return resp
         elif cmd == 'get_file':
-            path = self.dropBoxPath(kwargs.get('path',''))
             #print 'get_file', path
-            resp = self.client.get_file(self.dropBoxConfig['root'], path.strip())
+            resp = self.client.get_file(self.config['root'], path)
             return resp
         elif cmd == 'put_file':
-            path = self.dropBoxPath(kwargs.get('path',''))
-            #print 'put_file',  os.path.split(path)[0]
-            resp = self.client.put_file(self.dropBoxConfig['root'], os.path.split(path)[0], kwargs.get('data'))
+            #print 'put_file', os.path.split(path)[0]
+            resp = self.client.put_file(self.config['root'], os.path.split(path)[0], kwargs.get('data'))
             return resp
             
         return None
--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 # simple tests for DropBoxFs
-
+import os
 import sys
+
 
 from fs.dropboxfs import DropBoxFS
     
+ 
+
+    
 dropBoxFsConfig = {
-    'root':'dropbox'
-    ,'path':'/'
-    ,'consumer_key':'8px67m'
-    ,'consumer_secret':'o61w3o9'
-    ,'user_name':'julien@revolunet.com'
-    ,'user_password':'aTrP5xAhxS7'
+             'root':'dropbox'
+            ,'path':'/testcase1'
+            ,'consumer_key':'aaadffefz'
+            ,'consumer_secret':'zefzgehrh'
+            ,'user_name':'julien@revolunet.com'
+            ,'user_password':'1234567890'
 }
 
 c = DropBoxFS(**dropBoxFsConfig)
-
 
 print '[+]', 'list /'
 print c.listdir('/')
@@ -31,9 +34,9 @@ print 'ISDIR Public', c.isdir('Public')
 print 'ISFILE Public', c.isfile('Public')
 print 'ISDIR /various', c.isdir('/various')
 print 'ISFILE /various', c.isfile('/various')
-print 'ISDIR /various/', c.isdir('/various/')
-print 'ISFILE /various/', c.isfile('/various/')
-
+print 'ISDIR various', c.isdir('various')
+print 'ISFILE various', c.isfile('various')
+ 
 print '[+]', 'makedir test3'
 c.makedir('test3')
 print c.listdir('/')
@@ -47,21 +50,29 @@ print '[+]', 'rm test4'
 c.remove('test4')
 print c.listdir('/')
 
-print '[+]', 'write to sample222.txt'
-f = c.open('sample222.txt', 'w')
-f.write('My name is BatMan')
-f.close()
 
-print c.listdir('/')
+def fileReadWriteRemoveTest( c, filePath ):
+    print '[++] TEST fileReadWriteRemoveTest', filePath
+    print '\t', '[+]', 'write to %s' % filePath
+    f = c.open(filePath, 'w')
+    f.write('My name is BatMan in %s' % filePath)
+    f.close()
 
-print '[+]', 'read from sample222.txt'
-f = c.open('sample222.txt', 'r')
-print f.read()
-f.close()
+    print '\t', c.listdir(os.path.split(filePath)[0])
 
-print '[+]', 'rm sample222.txt'
-c.remove('sample222.txt')
-print c.listdir('/')
+    print '\t', '[+]', 'read from %s' % filePath
+    f = c.open(filePath, 'r')
+    print '\t', f.read()
+    f.close()
+
+    print '\t', '[+]', 'rm %s' % filePath
+    c.remove(filePath)
+    
+    print '\t', c.listdir(os.path.split(filePath)[0])
 
 
 
+fileReadWriteRemoveTest(c, 'testfile1.txt')
+fileReadWriteRemoveTest(c, 'testdir/testfile1.txt')
+c.remove('testdir')
+print '/ contains : ', c.listdir('/')
